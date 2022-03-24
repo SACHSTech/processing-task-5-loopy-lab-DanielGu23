0daeead3fdc076d0a9d675bc8ec823e0329f48b4
Daniel Gu <99457610+DanielGu23@users.noreply.github.com>
2022-03-24 16:43:06 +0000

    First Commit

--- Sketch.java
+++ Sketch.java
@@ -3,35 +3,24 @@ import processing.core.PApplet;
 public class Sketch extends PApplet {
 	
 	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+   /**
+  * A program Sketch.java that draws specific patterns in 8 different quadrants.
+  * @author: D. Gu
+  */
+  
   public void settings() {
-	// put your size call here
-    size(1200, 600);
+	  
+    // Size of the call
+    size(600, 300);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
     background(45, 150, 207);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    /*
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-*/
+    // Draw these functions
     draw_section_outlines();
     draw_section1();
     draw_section2();
@@ -42,8 +31,6 @@ public class Sketch extends PApplet {
     draw_section6();
     draw_section7();
     draw_section8();
-
-    
   }
 
 
@@ -54,35 +41,38 @@ public class Sketch extends PApplet {
     stroke(0);
     noFill();
 
-    // draw bottom row boxes
-    rect(0,300, 300, 300);
-    rect(300, 300, 300, 300);
-    rect(600, 300, 300, 300);
-    rect(900, 300, 300, 300);
+    // Draw bottom row boxes
+    rect(0,150, 150, 150);
+    rect(150, 150, 150, 150);
+    rect(300, 150, 150, 150);
+    rect(450, 150, 150, 150);
 
-    // draw top row boxes
-    rect(0,0, 300, 300);
-    rect(300, 0, 300, 300);
-    rect(600, 0, 300, 300);
-    rect(900, 0, 300, 300);
+    // Draw top row boxes
+    rect(0,0, 150, 150);
+    rect(150, 0, 150, 150);
+    rect(300, 0, 150, 150);
+    rect(450, 0, 150, 150);
   }
   
   /**
    * draws the bottom left section
    */
   public void draw_section1(){
-    int intX = 0;
-    int intY = 0;
+    float fltX = 0;
+    float fltY = 0;
+    
+    // Nested loop for 30 rows and 30 columns
+    for(int intColumn = 0; intColumn < 30; intColumn++){
+      for(int intRow = 0; intRow < 30; intRow++){
 
-    for(int intRow = 0; intRow < 30; intRow++){
-      for(int intColumn = 0; intColumn < 30; intColumn++){
-        intX = 3 + 0;  //Instead of zero, calculate the proper intX location using 'intRow'
-        intY = 300 + 3 + 0; //Instead of zero, calculate the proper intY location using 'intColumn'
+        // Add distance that boxes must move
+        fltX = 5/2 + (5 * intColumn);  
+        fltY = 305/2 + (5 * intRow); 
 
+        // Fill boxes with white
         fill(255);
         noStroke();
-        rect(intX, intY, 5, 5);
-
+        rect(fltX, fltY, (float)(5/2), (float)(5/2));
       }
     }
   }
@@ -92,7 +82,30 @@ public class Sketch extends PApplet {
    * Don't loop from 30 to 60 to shift everything over, just add 300 to x.
    */
   public void draw_section2(){
+    float fltX = 0;
+    float fltY = 0;
 
+    // Nested loop
+    for(int intColumn = 0; intColumn < 30; intColumn++){
+      for(int intRow = 0; intRow < 30; intRow++){
+        fltX = 305/2 + (5 * intColumn);  
+        fltY = 305/2 + (5 * intRow); 
+        
+        // Fill with white if column number is odd (starts at 0)
+        if (intColumn % 2 == 0){
+          fill(255);
+          noStroke();
+          rect(fltX, fltY, (float)(5/2), (float)(5/2));
+        }
+          
+        // Fill with black if column number is even
+        else{
+          fill(0);
+          noStroke();
+          rect(fltX, fltY, (float)(5/2), (float)(5/2));
+        }
+      }
+    }
   }
 
   /**
@@ -100,33 +113,132 @@ public class Sketch extends PApplet {
    * Don't use multiple 'if' statements.
    */
   public void draw_section3(){
+    float fltX = 0;
+    float fltY = 0;
 
+    for(int intColumn = 0; intColumn < 30; intColumn++){
+      for(int intRow = 0; intRow < 30; intRow++){
+        fltX = 605/2 + (5 * intColumn);  
+        fltY = 305/2 + (5 * intRow); 
+        
+        // Fill with black if row number is odd
+        if (intRow % 2 == 0){
+          fill(0);
+          noStroke();
+          rect(fltX, fltY, (float)(5/2), (float)(5/2));
+        }
+        
+        // Fill with white if row number is even
+        else{
+          fill(255);
+          noStroke();
+          rect(fltX, fltY, (float)(5/2), (float)(5/2));
+        }
+      }
+    }
   }
 
   /**
    * Use the modulus operator and just one 'if' statement to select the color.
    */
   public void draw_section4(){
+    float fltX = 0;
+    float fltY = 0;
 
+    for(int intColumn = 0; intColumn < 30; intColumn++){
+      for(int intRow = 0; intRow < 30; intRow++){
+        fltX = 905/2 + (5 * intColumn);  
+        fltY = 305/2 + (5 * intRow); 
+
+        // Fill with black if row number is odd
+        if (intRow % 2 == 0){
+          fill(0);
+          noStroke();
+          rect(fltX, fltY, (float)(5/2), (float)(5/2));
+        }
+
+        // Fill with black if row number and column number are even
+        else if((intRow % 2 != 0) && (intColumn % 2 != 0)){
+          fill(0);
+          noStroke();
+          rect(fltX, fltY, (float)(5/2), (float)(5/2));
+        }
+
+        // Fill all other rectangles with white
+        else{
+          fill(255);
+          noStroke();
+          rect(fltX, fltY, (float)(5/2), (float)(5/2));
+        }
+      }
+    }
   }
 
   /**
    * Do NOT use 'if' statements to complete 5-8. Manipulate the loops instead
    */
   public void draw_section5(){
+    float fltX = 0;
+    float fltY = 0;
 
+    // Draws triangle by manipulating inner loop's initial value
+    for(int intColumn = 29; intColumn >= 0; intColumn--){
+      for(int intRow = 29 - intColumn; intRow <= 30; intRow++){
+        fltX = 5/2 + (5 * intColumn);  
+        fltY = 5/2 + (5 * intRow); 
+        fill(255);
+        noStroke();
+        rect(fltX, fltY, (float)(5/2), (float)(5/2));
+      }
+    }
   }
 
   public void draw_section6(){
+    float fltX = 0;
+    float fltY = 0;
 
+    // Draws triangle by manipulating inner loop's lower limit
+    for(int intColumn = 29; intColumn >= 0; intColumn--){
+      for(int intRow = 29; intRow >= intColumn; intRow--){
+        fltX = 305/2 + (5 * intColumn);  
+        fltY = 5/2 + (5 * intRow); 
+        fill(255);
+        noStroke();
+        rect(fltX, fltY, (float)(5/2), (float)(5/2));
+      }
+    }
   }
 
   public void draw_section7(){
+    float fltX = 0;
+    float fltY = 0;
 
+    // Draws triangle by manipulating inner loop's initial value
+    for(int intColumn = 0; intColumn < 30; intColumn++){
+      for(int intRow = 29 - intColumn; intRow >= 0; intRow--){
+        fltX = 605/2 + (5 * intColumn);  
+        fltY = 5/2 + (5 * intRow); 
+        fill(255);
+        noStroke();
+        rect(fltX, fltY, (float)(5/2), (float)(5/2));
+      }
+    }
   }
   
   public void draw_section8(){
+    float fltX = 0;
+    float fltY = 0;
 
+    // Draws triangle by manipulating inner loop's lower limit
+    for(int intRow = 0; intRow < 30; intRow++){
+      for(int intColumn = 29; intColumn >= intRow; intColumn--){
+        fltX = 905/2 + (5 * intColumn);  
+        fltY = 5/2 + (5 * intRow); 
+        fill(255);
+        noStroke();
+        rect(fltX, fltY, (float)(5/2), (float)(5/2));
+      }
+    }
   }
 
 
